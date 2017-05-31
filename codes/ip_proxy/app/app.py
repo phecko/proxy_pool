671ded81b0dd02a79d66d3da8f1d649209c30549
linkaicong <linkaicong@linkaicongdeMacBook-Pro.local>
2017-05-31 23:40:53 +0800

    remove unuse file;independent setting file for dev/product

--- codes/ip_proxy/app/app.py
+++ codes/ip_proxy/app/app.py
@@ -1,21 +1,38 @@
 # -*- coding: utf-8 -*-
 import json
 import random
+import os
 
 from flask import Flask,request,g
 from pymongo import  *
 
 app = Flask(__name__)
 
-setting = {
-    "MongoHost": "mongo",
-    "MongoPort": 27017
-}
+
+def load_setting():
+    setting = {}
+    file_path = os.path.abspath( os.path.join(os.path.dirname(__file__),"../"))
+    if "PROXY_POOL_ENV" in os.environ and os.environ["PROXY_POOL_ENV"] == "product":
+        file_path = os.path.join(file_path, "product.conf")
+    else:
+        file_path = os.path.join(file_path, "dev.conf")
+
+    try:
+        with open(file_path,"rb") as f:
+            setting = json.load(f)
+    except Exception as e:
+        file_name = os.path.basename(file_path)
+        print "setting file %s is invalid\n %s" % (file_name,e)
+        os._exit(127)
+
+    return setting
+
+settings = load_setting()
 
 
 @app.before_request
 def connect_db():
-    g.db = MongoClient(setting["MongoHost"],setting["MongoPort"])
+    g.db = MongoClient(settings["MONGO_HOST"],settings["MONGO_PORT"])
 
 
 @app.teardown_request
